c10d2e854e7d6fc1308b50d1f79e36d82faeb321
George Adaimi <georgeadaimi@gmail.com>
2023-02-06 23:37:46 +0100

    Added old HRNet

--- src/openpifpaf/plugins/butterflydetector/__init__.py
+++ src/openpifpaf/plugins/butterflydetector/__init__.py
@@ -4,6 +4,8 @@ from . import headmeta
 from .heads import CompositeField
 from .losses.loss import CompositeLoss
 from .decoder import Butterfly
+from .hrnet import HRNet
+
 def register():
     openpifpaf.DATAMODULES['uavdt'] = UAVDTDataLoader
     openpifpaf.DATAMODULES['visdrone'] = VisdroneDataLoader
@@ -11,3 +13,7 @@ def register():
     openpifpaf.HEADS[headmeta.Butterfly] = CompositeField
     openpifpaf.LOSSES[headmeta.Butterfly] = CompositeLoss
     openpifpaf.DECODERS.add(Butterfly)
+
+    openpifpaf.BASE_TYPES.add(HRNet)
+    openpifpaf.BASE_FACTORIES['hrnetw32det'] = lambda: HRNet(cfg_file='w32_384x288_adam_lr1e-3.yaml', shortname='HRNet', detection=True, is_train=True)
+    openpifpaf.BASE_FACTORIES['hrnetw48det'] = lambda: HRNet(cfg_file='w48_384x288_adam_lr1e-3.yaml', shortname='HRNet', detection=True, is_train=True)
