8dffa98e7b7532cc07a0b48695c4abb62f8bd682
George Adaimi <georgeadaimi@gmail.com>
2023-04-04 12:51:04 +0200

    Added support for Laplace WH loss and fixed focal loss

--- src/openpifpaf/plugins/butterflydetector/headmeta.py
+++ src/openpifpaf/plugins/butterflydetector/headmeta.py
@@ -28,3 +28,24 @@ class Butterfly(openpifpaf.headmeta.Base):
     @property
     def n_fields(self):
         return len(self.keypoints)
+
+@dataclass
+class Butterfly_LaplaceWH(openpifpaf.headmeta.Base):
+    """Head meta data for a Composite Intensity Field (CIF)."""
+
+    keypoints: List[str]
+    categories: List[str]
+
+    n_confidences: ClassVar[int] = 1
+    n_vectors: ClassVar[int] = 2
+    n_scales: ClassVar[int] = 0
+
+    vector_offsets = [True]
+    decoder_min_scale = 0.0
+    decoder_seed_mask: List[int] = None
+
+    training_weights: List[float] = None
+
+    @property
+    def n_fields(self):
+        return len(self.keypoints)
